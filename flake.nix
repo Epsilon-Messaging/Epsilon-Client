f34025883080d6a64491368b4236bead492204bd
Tiqur <trevorbrage02@gmail.com>
2025-03-30 12:08:59 -0700

    Initial setup

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [  
+        gcc
+        cmake
+        clang
+        clang-tools
+        pkg-config
+      ];
+    };
+  };
+}
